8046d4697dac09b7dcb69ec7ccaa44d3a8e770fb
Ekta <Ekta@sheru-pc.wireless-guest.unc.edu>
2019-08-30 14:01:28 -0400

    final novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,34 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		
+		process(scan);
 		
 	}
+	
+	public static void process(Scanner scan) {
+
+		int count = scan.nextInt();
+
+		
+		for (int i = 0; i < count; i++) {
+			String first = scan.next();
+			String last = scan.next();
+			int items = scan.nextInt();
+			
+			double sum = 0; 
+			
+			for (int j = 0; j < items; j++) {
+				int quantity = scan.nextInt();
+				String name = scan.next();
+				double price = scan.nextDouble();
+				sum = sum + (quantity * price);
+			}
+			
+			System.out.println(first.substring(0, 1) + ". " + last + ": " + sum);
+		}
+		
+	
+	}
 }
+
