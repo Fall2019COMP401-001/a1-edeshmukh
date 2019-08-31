74b420a1edbd992ad65cd774ff0ddbcefd430c2b
Ekta <Ekta@sheru-pc.wireless-guest.unc.edu>
2019-08-30 21:59:54 -0400

    finished adept? console not working

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -8,7 +8,71 @@ public class A1Adept {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		process(scan);
 		
+	}
+	
+	public static void process(Scanner scan) {
+		int storeItems = scan.nextInt();
+		String itemName[] = new String[storeItems];
+		Double itemPrice[] = new Double[storeItems];
+		
+		for(int i = 0; i < storeItems; i++) {
+			itemName[i] = scan.next();
+			itemPrice[i] = scan.nextDouble();
+		}
+		
+		int customerNum = scan.nextInt();
+		String customers[] = new String[customerNum];
+		Double customerTotal[] = new Double[customerNum];
+		
+		for(int i = 0; i < customerNum; i++) {
+			String first = scan.next();
+			String last = scan.next();
+			customers[i] = first + " " + last;
+			double total = 0;
+			int itemsNumber = scan.nextInt();
+			
+			for(int j = 0; j < itemsNumber; j++) {
+				int item_num = scan.nextInt();
+				String item = scan.next();
+				
+				for(int x = 0; x < storeItems; x++) {
+					if(item.contentEquals(itemName[x])) {
+						double price = itemPrice[x];
+						price = price * item_num;
+						total = total + price;
+					}
+				}
+			}
+			customerTotal[i] = total;
+		}
+		
+		int maxCount = 0;
+		int minCount = 0;
+		double max = customerTotal[0];
+		double min = customerTotal[0];
+		double grandTotal = customerTotal[0];
+		
+		for(int y = 0; y < customerNum; y++) {
+			if(customerTotal[y] > max) {
+				max = customerTotal[y];
+				maxCount = y;
+			}
+			if(customerTotal[y] < min) {
+				min = customerTotal[y];
+				minCount = y;
+			}
+			grandTotal += customerTotal[y];
+		}
+		
+		double average = grandTotal/customerNum;
+		String firstAnswer = String.format("%,.2f",customerTotal[maxCount]);
+		String secondAnswer = String.format("%,.2f",customerTotal[minCount]);
+		String thirdAnswer = String.format("%,.2f",average);
+		
+		System.out.println("Biggest: " + customers[maxCount] + " (" + firstAnswer + ")" );
+		System.out.println("Smallest: " + customers[minCount] + " (" + secondAnswer + ")" );
+		System.out.println("Average: " + thirdAnswer);
 	}
 }
